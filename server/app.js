d8778d51d7f6adfb4c15aee4cd068cb92424ff7a
justtfelix <justttfelix@gmail.com>
2023-05-15 14:34:07 +0400

    [Server] Added endpoint to get user by ID

--- server/app.js
+++ server/app.js
@@ -31,9 +31,36 @@ app.get("/api/v1/users", (req, res) => {
       res.status(200).json({
         status: "success",
         count: users.length,
-        data: {
-          users,
-        },
+        data: users,
+      });
+    }
+  });
+});
+
+// Get user by ID
+app.get("/api/v1/users/:userId", (req, res) => {
+  const userId = parseInt(req.params.userId);
+  
+  fs.readFile(dataPath, "utf-8", (err, data) => {
+    if (err) {
+      res.status(500).json({
+        status: "fail",
+        message: "An error occurred while reading the data",
+      });
+    }
+
+    const users = JSON.parse(data);
+    const user = users.find((user) => user.id === userId);
+    
+    if (!user) {
+      res.status(404).json({
+        status: "fail",
+        message: `User with ID ${userId} not found`,
+      });
+    } else {
+      res.status(200).json({
+        status: "success",
+        data: user,
       });
     }
   });
