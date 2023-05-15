8db57de3e5be42f56e6c2ccea112b37c3f15266d
justtfelix <justttfelix@gmail.com>
2023-05-15 17:42:23 +0400

    [Server] Added endpoint to delete user by ID

--- server/app.js
+++ server/app.js
@@ -41,7 +41,7 @@ app.get("/api/v1/users", (req, res) => {
 // Get user by ID
 app.get("/api/v1/users/:userId", (req, res) => {
   const userId = parseInt(req.params.userId);
-  
+
   fs.readFile(dataPath, "utf-8", (err, data) => {
     if (err) {
       return res.status(500).json({
@@ -52,7 +52,7 @@ app.get("/api/v1/users/:userId", (req, res) => {
 
     const users = JSON.parse(data);
     const user = users.find((user) => user.id === userId);
-    
+
     if (!user) {
       res.status(404).json({
         status: "fail",
@@ -125,7 +125,7 @@ app.patch("/api/v1/users/:userId", (req, res) => {
     const updatedUser = Object.assign(userToUpdate, req.body);
 
     users[index] = updatedUser;
-    
+
     fs.writeFile(dataPath, JSON.stringify(users), (err) => {
       if (err) {
         res.status(500).json({
@@ -136,6 +136,47 @@ app.patch("/api/v1/users/:userId", (req, res) => {
         res.status(200).json({
           status: "success",
           data: updatedUser,
+        });
+      }
+    });
+  });
+});
+
+// Delete user by ID
+app.delete("/api/v1/users/:userId", (req, res) => {
+  const userId = parseInt(req.params.userId);
+
+  fs.readFile(dataPath, "utf-8", (err, data) => {
+    if (err) {
+      return res.status(500).json({
+        status: "fail",
+        message: "An error occurred while reading the data",
+      });
+    }
+
+    const users = JSON.parse(data);
+    const userToDelete = users.find((user) => user.id === userId);
+
+    if (!userToDelete) {
+      return res.status(404).json({
+        status: "fail",
+        message: `User with ID ${userId} not found`,
+      });
+    }
+
+    const index = users.indexOf(userToDelete);
+    users.splice(index, 1);
+
+    fs.writeFile(dataPath, JSON.stringify(users), (err) => {
+      if (err) {
+        res.status(500).json({
+          status: "fail",
+          message: "An error occurred while writing the data",
+        });
+      } else {
+        res.status(200).json({
+          status: "success",
+          message: `User with ID ${userId} deleted successfully. The user's data has been permanently removed from our system and cannot be restored`,
         });
       }
     });
