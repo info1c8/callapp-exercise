d9b90a89cc4f6188dc25b9455654b5e77b1b863c
justtfelix <justttfelix@gmail.com>
2023-05-15 14:00:39 +0400

    [Server] Added endpoint to get all users

--- server/app.js
+++ server/app.js
@@ -2,9 +2,11 @@ import express from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import cors from "cors";
+import fs from "fs";
 
 dotenv.config();
 
+// Setup express app
 const app = express();
 const { PORT } = process.env;
 
@@ -12,8 +14,29 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
+// The path to the JSON file that contains the data
+const dataPath = "./data/users.json";
+
+// Define the routes
+// Get all users
+app.get("/api/v1/users", (req, res) => {
+  fs.readFile(dataPath, "utf-8", (err, data) => {
+    if (err) {
+      res.status(500).json({
+        status: "fail",
+        message: "An error occurred while reading the data",
+      });
+    } else {
+      const users = JSON.parse(data);
+      res.status(200).json({
+        status: "success",
+        count: users.length,
+        data: {
+          users,
+        },
+      });
+    }
+  });
 });
 
 app.listen(PORT, () => {
