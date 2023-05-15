478b32715f29dc70f98fc687f283354e148dd109
justtfelix <justttfelix@gmail.com>
2023-05-15 12:32:49 +0400

    [Server] Added HTTP request logger middleware

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import morgan from "morgan";
 import cors from "cors";
 
 dotenv.config();
@@ -8,6 +9,7 @@ const app = express();
 const { PORT } = process.env;
 
 app.use(cors());
+app.use(morgan("dev"));
 app.use(express.json());
 
 app.get("/", (req, res) => {
