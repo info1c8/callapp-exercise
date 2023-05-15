a45881a170be7f880e00dad5938fb0258fad7f0c
justtfelix <justttfelix@gmail.com>
2023-05-15 12:29:23 +0400

    [Server] Added cors and json body parser middlewares

--- server/app.js
+++ server/app.js
@@ -1,10 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const { PORT } = process.env;
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
