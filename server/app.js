bd35861da19caf815b2c9bf1eb28eb731c34653c
justtfelix <justttfelix@gmail.com>
2023-05-15 17:14:53 +0400

    [Server] Added endpoint to update user

--- server/app.js
+++ server/app.js
@@ -44,7 +44,7 @@ app.get("/api/v1/users/:userId", (req, res) => {
   
   fs.readFile(dataPath, "utf-8", (err, data) => {
     if (err) {
-      res.status(500).json({
+      return res.status(500).json({
         status: "fail",
         message: "An error occurred while reading the data",
       });
@@ -67,10 +67,11 @@ app.get("/api/v1/users/:userId", (req, res) => {
   });
 });
 
+// Create user
 app.post("/api/v1/users", (req, res) => {
   fs.readFile(dataPath, "utf-8", (err, data) => {
     if (err) {
-      res.status(500).json({
+      return res.status(500).json({
         status: "fail",
         message: "An error occurred while reading the data",
       });
@@ -92,6 +93,49 @@ app.post("/api/v1/users", (req, res) => {
         res.status(201).json({
           status: "success",
           data: newUser,
+        });
+      }
+    });
+  });
+});
+
+// Update user by ID
+app.patch("/api/v1/users/:userId", (req, res) => {
+  const userId = parseInt(req.params.userId);
+
+  fs.readFile(dataPath, "utf-8", (err, data) => {
+    if (err) {
+      return res.status(500).json({
+        status: "fail",
+        message: "An error occurred while reading the data",
+      });
+    }
+
+    const users = JSON.parse(data);
+    const userToUpdate = users.find((user) => user.id === userId);
+
+    if (!userToUpdate) {
+      return res.status(404).json({
+        status: "fail",
+        message: `User with ID ${userId} not found`,
+      });
+    }
+
+    const index = users.indexOf(userToUpdate);
+    const updatedUser = Object.assign(userToUpdate, req.body);
+
+    users[index] = updatedUser;
+    
+    fs.writeFile(dataPath, JSON.stringify(users), (err) => {
+      if (err) {
+        res.status(500).json({
+          status: "fail",
+          message: "An error occurred while writing the data",
+        });
+      } else {
+        res.status(200).json({
+          status: "success",
+          data: updatedUser,
         });
       }
     });
