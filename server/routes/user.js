4a161301dcc16e3c92464d72b827ff2b09ced073
justtfelix <justttfelix@gmail.com>
2023-05-18 01:31:21 +0400

    [Server] Changed controllers file name

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
-import { createUser } from "../controllers/user/post.js";
-import { updateUser } from "../controllers/user/patch.js";
-import { getAllUsers, getUserById } from "../controllers/user/get.js";
+import { createUser } from "../controllers/user/create.js";
+import { updateUser } from "../controllers/user/update.js";
+import { getAllUsers, getUserById } from "../controllers/user/read.js";
 import { deleteAllUsers, deleteUserById } from "../controllers/user/delete.js";
 
 export const userRouter = Router();
