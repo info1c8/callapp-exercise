099ce3ebecdc21574318da47e40b931b280bb654
justtfelix <justttfelix@gmail.com>
2023-05-19 19:55:11 +0400

    [Server] HTML file is rendered

--- server/app.js
+++ server/app.js
@@ -2,7 +2,12 @@ import express from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 import { userRouter } from "./routes/user.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config();
 
@@ -16,6 +21,10 @@ app.use(express.json());
 
 // Route middleware
 app.use("/api/v1", userRouter);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
